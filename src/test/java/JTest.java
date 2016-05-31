ef787bce4b2111128d03cc828d727c4903a3f6f8
wys43cc <ykh1989126@126.com>
2016-05-31 19:36:52 +0800

    this is the first commit

--- src/test/java/JTest.java
+++ src/test/java/JTest.java
@@ -5,5 +5,6 @@ public class JTest {
     public static void main(String[] args) {
         String str = new String();
         System.out.println("args = [" + args + "]");
+        System.out.println("args = [" + args + "]");
     }
 }
